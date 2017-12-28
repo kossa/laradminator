03bae0aac033b7a8a9dabafdb480714b3fbc5ce3
kouceyla <hadjikouceyla@gmail.com>
2017-12-28 21:41:30 +0100

    Intgration assets of adminator

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -10,7 +10,6 @@ window._ = require('lodash');
 try {
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap-sass');
 } catch (e) {}
 
 /**
@@ -37,19 +36,19 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
 
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key',
-//     cluster: 'mt1',
-//     encrypted: true
-// });
+import './masonry';
+import './charts';
+import './popover';
+import './scrollbar';
+import './search';
+import './sidebar';
+import './skycons';
+import './vectorMaps';
+import './chat';
+import './datatable';
+import './datepicker';
+import './email';
+import './fullcalendar';
+import './googleMaps';
+import './utils';
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets/static/images','public/images')
+    .version()
+    .sourceMaps();
