c4dfa511b5736240bd153c8d5e824b4e70ebe54e
kouceyla <hadjikouceyla@gmail.com>
2017-12-30 14:23:11 +0100

    Close admin

--- resources/views/admin/partials/topbar.blade.php
+++ resources/views/admin/partials/topbar.blade.php
@@ -181,10 +181,10 @@
       <li class="dropdown">
         <a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
           <div class="peer mR-10">
-            <img class="w-2r bdrs-50p" src="/images/1.jpg" alt="">
+            <img class="w-2r bdrs-50p" src="{{ auth()->user()->avatar }}" alt="">
           </div>
           <div class="peer">
-            <span class="fsz-sm c-grey-900">John Doe</span>
+            <span class="fsz-sm c-grey-900">{{ auth()->user()->name }}</span>
           </div>
         </a>
         <ul class="dropdown-menu fsz-sm">
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Auth::routes();
 | Admin
 |------------------------------------------------------------------------------------
 */
-Route::group(['prefix' => ADMIN, 'as' => ADMIN . '.'], function() {
+Route::group(['prefix' => ADMIN, 'as' => ADMIN . '.', 'middleware'=>['auth', 'Role:0']], function() {
     Route::get('/', 'DashboardController@index')->name('dash');
     Route::resource('users', 'UserController');
 });
