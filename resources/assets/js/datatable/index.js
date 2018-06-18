48c544b30934a5f49eb0bf81125fbd1191a512fb
kouceyla <hadjikouceyla@gmail.com>
2018-06-18 22:58:20 +0100

    Datatable - Add indication for languages fix #6

--- resources/assets/js/datatable/index.js
+++ resources/assets/js/datatable/index.js
@@ -2,5 +2,11 @@ import * as $ from 'jquery';
 import 'datatables';
 
 export default (function () {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+		language: {
+			// 'url' : 'https://cdn.datatables.net/plug-ins/1.10.16/i18n/French.json'
+			// More languages : http://www.datatables.net/plug-ins/i18n/
+		},
+		aaSorting: []
+	});
 }());
