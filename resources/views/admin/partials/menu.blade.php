3f0f13dbc71c0d041015d99525dc3cff6b31ab77
kouceyla <hadjikouceyla@gmail.com>
2018-11-28 18:56:59 +0100

    Classroom - index fix #89

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -1,5 +1,5 @@
-<li class="nav-item mT-30 active">
-    <a class='sidebar-link' href="{{ route(ADMIN . '.dash') }}" default>
+<li class="nav-item mT-30">
+    <a class='sidebar-link active' href="{{ route(ADMIN . '.dash') }}" default>
         <span class="icon-holder">
             <i class="c-blue-500 ti-home"></i>
         </span>
