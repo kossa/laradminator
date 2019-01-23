6e1debf100a70435c6e481bebaa5b10d1b156256
kouceyla <hadjikouceyla@gmail.com>
2019-01-23 10:03:29 +0100

    Update composer and npm

--- resources/js/sidebar/index.js
+++ resources/js/sidebar/index.js
@@ -45,16 +45,16 @@ export default (function () {
   // Sidebar Activity Class
   const sidebarLinks = $('.sidebar').find('.sidebar-link');
 
-  // sidebarLinks
-  //   .each((index, el) => {
-  //     // $(el).removeClass('active');
-  //   })
-  //   .filter(function () {
-  //     const href = $(this).attr('href');
-  //     const pattern = href[0] === '/' ? href.substr(1) : href;
-  //     return pattern === (window.location.pathname).substr(1);
-  //   })
-  //   .addClass('active');
+  sidebarLinks
+    .each((index, el) => {
+      $(el).removeClass('active');
+    })
+    .filter(function () {
+      const href = $(this).attr('href');
+      const pattern = href[0] === '/' ? href.substr(1) : href;
+      return pattern === (window.location.pathname).substr(1);
+    })
+    .addClass('active');
 
   // ٍSidebar Toggle
   $('.sidebar-toggle').on('click', e => {
