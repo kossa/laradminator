61e3c1270b82b7b17e2e15decac9ce2118b551b4
kouceyla <hadjikouceyla@gmail.com>
2019-07-07 08:31:43 +0100

    rprod

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ require('./bootstrap');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
