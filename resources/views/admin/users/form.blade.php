272debeeef9a26191110eb05af755698f12204e8
kouceyla <hadjikouceyla@gmail.com>
2019-07-07 08:32:53 +0100

    user - form - fix indent

--- resources/views/admin/users/form.blade.php
+++ resources/views/admin/users/form.blade.php
@@ -3,17 +3,17 @@
 		<div class="bgc-white p-20 bd">
 			{!! Form::myInput('text', 'name', 'Username') !!}
 		
-				{!! Form::myInput('email', 'email', 'Email') !!}
-		
-				{!! Form::myInput('password', 'password', 'Password') !!}
-		
-				{!! Form::myInput('password', 'password_confirmation', 'Password again') !!}
-		
-				{!! Form::mySelect('role', 'Role', config('variables.role'), null, ['class' => 'form-control select2']) !!}
-		
-				{!! Form::myFile('avatar', 'Avatar') !!}
-		
-				{!! Form::myTextArea('bio', 'Bio') !!}
+			{!! Form::myInput('email', 'email', 'Email') !!}
+	
+			{!! Form::myInput('password', 'password', 'Password') !!}
+	
+			{!! Form::myInput('password', 'password_confirmation', 'Password again') !!}
+	
+			{!! Form::mySelect('role', 'Role', config('variables.role'), null, ['class' => 'form-control select2']) !!}
+	
+			{!! Form::myFile('avatar', 'Avatar') !!}
+	
+			{!! Form::myTextArea('bio', 'Bio') !!}
 		</div>  
 	</div>
 </div>
