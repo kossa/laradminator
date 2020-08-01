309798d046872d3c3743330e5fbb32fa6ccba19d
Zemmouri Rediane <z.rediane24@gmail.com>
2020-08-01 17:36:50 +0100

    Refactor head into reusable template

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -1,22 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-	<link href="{{ mix('/css/app.css') }}" rel="stylesheet"> 
-	{{-- <link href="{{ mix('/css/rtl.css') }}" rel="stylesheet">  --}}
-	
-	@yield('css')
-
-</head>
+@include('layouts.partials.head')
 
 <body class="app">
 
@@ -38,8 +23,8 @@
 
                         <h4 class="c-grey-900 mT-10 mB-30">@yield('page-header')</h4>
 
-						@include('admin.partials.messages') 
-						@yield('content')
+                        @include('admin.partials.messages')
+                        @yield('content')
 
                     </div>
                 </div>
