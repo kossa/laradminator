5c0b8c93f29c067c29afa22001b88660cf472a5d
Zemmouri Rediane <z.rediane24@gmail.com>
2020-08-16 21:15:17 +0100

    RTL - fix checkbox labels

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Styles -->
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
-    {{-- <link href="{{ mix('/css/rtl.css') }}" rel="stylesheet"> --}}
+    <link href="{{ mix('/css/rtl.css') }}" rel="stylesheet">
 
     @yield('css')
 
