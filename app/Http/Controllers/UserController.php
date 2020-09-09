047ee40a726e54c23ba152e68bff384143a63947
Zemmouri Rediane <z.rediane24@gmail.com>
2020-09-09 20:18:08 +0100

    Put User model into Models directory to fit with Laravel 8.0

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\User;
+use App\Models\User;
 
 class UserController extends Controller
 {
