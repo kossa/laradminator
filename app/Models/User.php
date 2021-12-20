5cd14d429f983038d2001c52d839a812e9ea03b3
kouceyla <hadjikouceyla@gmail.com>
2021-12-20 09:12:13 +0100

    update yarn

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,7 +72,7 @@ class User extends Authenticatable
     public function getAvatarAttribute($value)
     {
         if (!$value) {
-            return 'https://placehold.it/160x160';
+            return '/images/avatar.png';
         }
 
         return config('variables.avatar.public').$value;
