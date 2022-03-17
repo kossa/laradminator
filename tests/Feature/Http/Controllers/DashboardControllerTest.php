d55d86ffbd9e0d95a92cbe59ac50361f30590bc7
kouceyla <hadjikouceyla@gmail.com>
2022-03-17 11:39:10 +0100

    Pest: Dashboad done

---
+++ tests/Feature/Http/Controllers/DashboardControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use function Pest\Laravel\actingAs;
+
+uses(RefreshDatabase::class);
+
+test('Admin can access to dashboard page', function () {
+    $user = User::factory()->create(['role' => 10]);
+    actingAs($user);
+
+    $this->get(ADMIN . '/')->assertOk();
+});
+
+test('Non admin cannot access to dashboard page', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $this->get(ADMIN . '/')->assertNotFound();
+});
